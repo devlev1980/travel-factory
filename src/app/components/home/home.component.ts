d533db8a9952704b50ac5509cfb31b84ea31fbd8
Yevgeny Levin <devlev1980@gmail.com>
2020-03-08 14:36:36 +0200

    add project

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,88 @@
+import {AfterViewChecked, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ContactService} from '../../services/contact.service';
+import {IContact} from '../../models/contact';
+import {MatDialog} from '@angular/material';
+import {AddDialogComponent} from '../../dialogs/add-dialog/add-dialog.component';
+import {YesNoDialogComponent} from '../../dialogs/yes-no-dialog/yes-no-dialog.component';
+import {EditDialogComponent} from '../../dialogs/edit-dialog/edit-dialog.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit, AfterViewChecked {
+  contacts: IContact[] = [];
+
+  constructor(private contactService: ContactService,
+              public dialog: MatDialog,
+              private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+   this.contactService.getContacts().subscribe((data) => {
+      this.contacts = data;
+    });
+
+    // this.contactService.getContacts().pipe(
+    //   switchMap(contacts => {
+    //     contacts.forEach((contact) => {
+    //       return this.contactService.getCoordinates(contact.city);
+    //     });
+    //     return contacts;
+    //   })
+    // ).subscribe((data) => {
+    //   this.contacts.push(data);
+    //   this.contacts.forEach(res => {
+    //     res.location.lon = data.location.lon;
+    //   });
+    // });
+  }
+
+  ngAfterViewChecked() {
+    this.cdr.detectChanges();
+  }
+
+
+  onAddContact() {
+    const dialogRef = this.dialog.open(AddDialogComponent, {
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe(contact => {
+      if (contact) {
+        this.contacts.push(contact);
+      }
+    });
+  }
+
+  onDeleteContact(contactId) {
+    const dialogRef = this.dialog.open(YesNoDialogComponent, {
+      width: '300px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'Yes') {
+        this.contacts.splice(contactId, 1);
+
+      }
+    });
+  }
+
+  onEditContact(contact: IContact, index) {
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '600px',
+      data: contact
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.contacts[index] = result;
+
+      } else {
+        return;
+      }
+    });
+  }
+
+
+
+}
